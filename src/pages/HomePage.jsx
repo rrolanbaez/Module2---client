e65696f9d37cc3b1514e7e0f026b1a045d8c57cc
Roxangelica Rolan <rrolanbaez@gmail.com>
2024-03-12 19:42:22 -0400

    implemented some CSS in HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,47 @@
-const imgURL = " https://cdn3.vectorstock.com/i/1000x1000/59/42/ecommerce-icon-vector-4525942.jpg " ;
-function HomePage() {
 
+// const imgURL = "https://e0.pxfuel.com/wallpapers/40/826/desktop-wallpaper-puerto-rico-beach-puerto-rico-beach-sunset.jpg" ;
+// function HomePage() {
+
+//   return (
+//     <div className="w-">
+//       <h1>Home</h1>
+//       <img src={imgURL} alt="home gif" className="page-img" />
+//     </div>
+//   );
+// }
+
+// export default HomePage;
+
+import React from 'react';
+
+const imgURL = "https://e0.pxfuel.com/wallpapers/40/826/desktop-wallpaper-puerto-rico-beach-puerto-rico-beach-sunset.jpg";
+
+function HomePage() {
   return (
-    <div className="w-">
-      <h1>Home</h1>
-      <img src={imgURL} alt="home gif" className="page-img" />
+    <div>
+      <div className="img-container">
+        <img src={imgURL} alt="home" className="page-img" />
+        <h1 className="centered ojuju-custom"><strong>Coquí Compass</strong></h1>
+        <p className="centered-text ojuju-custom">Your Gateway to Adventure in Puerto Rico</p>
+      </div>
+
+      
+
+      <div className="content">
+        {/* Add more content here that will appear as you scroll down */}
+        <p>This is additional content that will appear as you scroll down.</p>
+        {/* Example of using Bootstrap for layout */}
+        <div className="container mt-5">
+          <div className="row">
+            <div className="col">
+              <p>More content on the left</p>
+            </div>
+            <div className="col">
+              <p>More content on the right</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
